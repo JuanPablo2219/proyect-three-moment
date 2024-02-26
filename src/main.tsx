9dcac1083fd7b688b47cd296a705419847e5bcc0
JuanPablo2219 <jpmalla@sudamericano.edu.ec>
2024-02-26 13:22:01 -0500

    Restore de repositorio

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Form from './components/Form'
+import App from './App'
         
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <h2 style={ { textAlign:'center',color:'white'}}>Bandera Tricolor</h2>
+    <App/>
+    <br />
+    <br />
+    
+    <h1 style={ { textAlign:'center',color:'white'}}>Formulario de Local Storage</h1>
     <Form />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
